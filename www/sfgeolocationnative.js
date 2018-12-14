211127b6704713b1c069109b6540375669cbeb92
chrystiadi <45218892+chrystiadi@users.noreply.github.com>
2018-12-14 16:11:16 +0700

    Update sfgeolocationnative.js

--- www/sfgeolocationnative.js
+++ www/sfgeolocationnative.js
@@ -22,4 +22,28 @@ PositionResult:
 - longitude
 - accuracy
 - location_provider
+*//**
+ *  latitude and longitude to find location address.
+ * @param {*} success {PositionResult[]} Success callback containing array of result objects
+ * @param {*} error Error callback
+ */
+var sfgeolocation = {
+  getCurrentLocation: function (success, error) {
+    cordova.exec(success, error, 'SFGeolocationNative', 'getCurrentLocation', []);
+  }
+}
+
+cordova.addConstructor(function () {
+  if (!window.plugins) {window.plugins = {};}
+
+  window.plugins.sfgeolocationnative = sfgeolocation;
+  return window.plugins.sfgeolocationnative;
+});
+
+/*
+PositionResult:
+- latitude
+- longitude
+- accuracy
+- location_provider
 */
